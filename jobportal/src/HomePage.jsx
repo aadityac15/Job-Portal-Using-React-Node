0d1532ad8ab7333824685f6da338381b1e5d4133
aadityac1511 <achokshi@mail.sfsu.edu>
2019-06-25 19:05:17 -0400

    Third Commit

--- jobportal/src/HomePage.jsx
+++ jobportal/src/HomePage.jsx
@@ -3,8 +3,8 @@ import './homepage.css';
 export default class HomePage extends React.Component {
     render() {
         return (
-            <div className="home-div-main">
-                <div className='home-div-heading'>
+            <div className="home-div-main container">
+                <div>
                     <h1>Job Portal</h1>
                     <h3>Current Jobs</h3>
                 </div>
@@ -26,6 +26,12 @@ export default class HomePage extends React.Component {
                             <li>
                                 <h4>Marketer</h4>
                                 <p>Need someone to promote my startup site!</p>
+                            </li>
+                        </div>
+                        <div className = 'home-div-list'>
+                            <li>
+                                <h4>Security Engineer</h4>
+                                <p>Security for a website.</p>
                             </li>
                         </div>
                     </ol>
--- jobportal/src/JobPortal.jsx
+++ jobportal/src/JobPortal.jsx
@@ -12,24 +12,22 @@ export default class JobPortal extends React.Component {
             <div>
                 <Router>
                     <div className="header">
-                        <div className='navigation-link'>
-
-                            <div className='navigation-div full-height display-flex align-items-center'>
-
-                                <Link to="/"><img src="../public/Image.jpg" alt="Logo" /> </Link>
-                                <Link to='/searchjobs/'><b>Search For Jobs</b></Link>
-                                <Link to='/postjobs/'><b>Post a Job</b></Link>
-                                <Link to='/myprofile/'><b>My Profile</b></Link>
-
+                        <div className='navigation-header container'>
+                            <div className='navigation-div .nav nav-tabs'>
+                                <span className='navigation-span'>
+                                    <Link to="/" ><img src="../public/Image.jpg" alt="Logo" /> </Link> &nbsp;
+                                    <Link to="/" ><b className = '.nav-justified'>Home</b></Link>
+                                    <Link to='/searchjobs/'     ><b>Search For Jobs</b></Link>
+                                    <Link to='/postjobs/'><b>Post a Job</b></Link>
+                                    <Link to='/myprofile/'><b>My Profile</b></Link>
+                                </span>
                             </div>
+                        </div>
+                        <Route path="/" exact component={HomePage} />
+                        <Route path="/searchjobs/" component={SearchJobs} />
+                        <Route path="/postjobs/" component={PostJobs} />
+                        <Route path="/myprofile/" component={MyProfile} />
 
-                        </div>
-                        <div>
-                            <Route path="/" exact component={HomePage} />
-                            <Route path="/searchjobs/" component={SearchJobs} />
-                            <Route path="/postjobs/" component={PostJobs} />
-                            <Route path="/myprofile/" component={MyProfile} />
-                        </div>
                     </div>
                 </Router>
             </div>
